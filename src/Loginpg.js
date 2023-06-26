b43d264424c70f419caf77ebcd5f415a9d17a68a
Nikhil Mane <nikhil@duretechnologies.com>
2023-06-26 15:18:59 +0530

    design changes

--- src/Loginpg.js
+++ src/Loginpg.js
@@ -11,6 +11,10 @@ import axios from "axios";
 import Appnavbar from './Appnavbar';
 import { useSelector, useDispatch } from 'react-redux'
 import { settoken } from './Reducers/appReducer';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 export default function Loginpg(props) {
     const { window } = props;
@@ -51,13 +55,15 @@ export default function Loginpg(props) {
     return (
         <Box sx={{ display: 'flex' }}>
             <Appnavbar/>
-            <Box component="main" className='MainContainer' sx={{ p: 0, width: '100%' }}>
+            <Box component="main" className='MainContainer loginpage' sx={{ p: 0, width: '100%' }}>
                 <Toolbar />
                 <Grid container spacing={2} className='loginpagemaindiv'>
                     {/* <Grid item xs={3} className='Logocontainer'>
                         <div className='Logodiv'></div>
                     </Grid> */}
+                   
                     <Grid item xs={12}>
+                    <p className='applicationtitle'>Welcome to the RBM Gateway of UNFPA's India Country Office</p>
                         <Card sx={{ maxWidth:"50%", margin:"0 auto", marginTop:"0px" }}>
                         <p className='formtitle '>Login</p>
                             <CardContent>
@@ -77,6 +83,34 @@ export default function Loginpg(props) {
                            
                         </Card>
                     </Grid>
+                </Grid>
+                <Grid container spacing={2} className='footerbg'>
+                <Grid item xs={3}>
+                    <div className='footerlogo'></div>
+                   
+                </Grid>
+                <Grid item xs={3}>
+                <Grid container spacing={2} className=''>
+                <Grid item xs={4}> <p className='copyright pt-0px'> Follow us</p></Grid>
+                <Grid item xs={8} className='sociallogoholder'>
+                <a href='https://twitter.com/UNFPAIndia' target='_blank'><TwitterIcon></TwitterIcon></a>
+                    <a href="https://www.facebook.com/unfpaindia"  target='_blank'><FacebookIcon></FacebookIcon></a>
+                    <a href="https://www.youtube.com/channel/UCaktHdd02Iyl1nfePglvAuw" target='_blank'><YouTubeIcon></YouTubeIcon></a>
+                    <a href="https://www.instagram.com/unfpaindia/" target='_blank'><InstagramIcon></InstagramIcon></a>
+                </Grid>
+                </Grid>
+                   
+               
+                </Grid>
+                <Grid item xs={3}>
+                    <a href='https://www.unfpa.org/donate#india' target='_blank' className='donatelink'><span className='donatelogo'></span>
+                    <span className='donatetext'>Donate</span>
+                    </a>
+                </Grid>
+                
+                <Grid item xs={3}>
+                <p className='copyright pt-0px'> © All rights reserved. 2023</p>
+                </Grid>
                 </Grid>
             </Box>
 
