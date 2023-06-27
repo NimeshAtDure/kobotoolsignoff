1488bec2986d8e63d756afe3d698eb4535a8ea79
Nikhil Mane <nikhil@duretechnologies.com>
2023-06-27 12:24:43 +0530

    login page design changes

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 export default function Footer(){
     return (
         <Grid container spacing={2} className='footerbg'>
-                <Grid item xs={6} md={3} sm={3}>
+                {/* <Grid item xs={6} md={3} sm={3}>
                     <div className='footerlogo'></div>
                    
                 </Grid>
@@ -28,9 +28,9 @@ export default function Footer(){
                     <a href='https://www.unfpa.org/donate#india' target='_blank' className='donatelink'><span className='donatelogo'></span>
                     <span className='donatetext'>Donate</span>
                     </a>
-                </Grid>
+                </Grid> */}
                 
-                <Grid item xs={12} md={3} sm={3}>
+                <Grid item xs={12} md={3} sm={3} className="pt-0px">
                 <p className='copyright pt-0px'> © All rights reserved. 2023</p>
                 </Grid>
                 </Grid>
