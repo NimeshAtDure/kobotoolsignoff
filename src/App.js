d1af2b9d4e2c669adefd9baea06bfa8619775edb
NimeshAtDure <nimesh.naik@duretechnologies.com>
2023-06-29 16:43:18 +0530

    sign off flow changes

--- src/App.js
+++ src/App.js
@@ -32,17 +32,17 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={token ? <Navigate to="/forms" replace={true} /> : <Navigate to="/login" replace={true} />} />
+        {/* <Route path="/" element={token ? <Navigate to="/forms" replace={true} /> : <Navigate to="/login" replace={true} />} />
         <Route path="/login" element={token ? <Navigate to="/forms" replace={true} /> : getElement("login")}/>
         <Route path="/forms" element={!token ? <Navigate to="/login" replace={true} /> : getElement("forms")} />
         <Route path="/signoff" element={getElement("signoff")} />
-        <Route path="/dashboard" element={getElement("dashboard")} />
+        <Route path="/dashboard" element={getElement("dashboard")} /> */}
 
-        {/* <Route path="/" element={token ? <Navigate to="/dashboard" replace={true} /> : <Navigate to="/login" replace={true} />} />
+        <Route path="/" element={token ? <Navigate to="/dashboard" replace={true} /> : <Navigate to="/login" replace={true} />} />
         <Route path="/login" element={token ? <Navigate to="/dashboard" replace={true} /> : getElement("login")}/>
         <Route path="/dashboard" element={!token ? <Navigate to="/login" replace={true} /> : getElement("dashboard")} />
         <Route path="/signoff" element={getElement("signoff")} />
-        <Route path="/forms" element={getElement("forms")} /> */}
+        <Route path="/forms" element={getElement("forms")} />
       </Routes>
     </div>
   );
